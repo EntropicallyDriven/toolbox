89c64469d232ea2adcfeedcee9dec0f247e2339a
Joshua Straquadine <jstraquadine@gmail.com>
2015-09-15 18:44:04 -0700

    matbook: Fixed up formatting and added the rest of the methods

--- matbook.py
+++ matbook.py
@@ -1,40 +1,60 @@
 class Mat:
+
     def __init__(self, matname):
 
-        self._knownmats = ['Al','OFE_Cu']
+        self.MB_INDEX = ['Al',
+                         'OFE_Cu',
+                         'BeCu']
 
-        if matname in self.knownmats:
-            self._name = matname
-        else:
-            self._name = 'unknown'
-        self._density = None
+        self.name         =     matname
 
-        self._specheat = None
-        self._tcond = None
+        ############  MECHANICAL ########################
+        self._density     =     None    #kg/m^3
 
-        self._resist = None
+        ############   THERMAL   ########################
+        self._specheat    =     None    #J/kg
+        self._tcond       =     None    #W/m K
+        self.tmelt        =     None
 
-        self._emis_pol = None
-        self._emis_ox = None
+        ############  ELECTRICAL ########################
+        self._resist      =     None    #Ohm m
+
+        ############   OPTICAL   ########################
+        self._emis_pol    =     None
+        self._emis_ox     =     None
 
 
 
         if matname == 'Al':
-            self._name = 'Aluminum'
-            self._density = 27000.0    #kg/m
 
-            self._specheat = 902.0     #J/kg
-            self._tcond = 205.0        #W/m K
+            self.name         =     'Aluminum'
+            self._density     =     2700.0
 
-            self._resist = 2.65e-8     #Ohm/m
+            self._specheat    =     902.0
+            self._tcond       =     205.0
+            self._tmelt       =     933.7
 
-            self._emis_pol = 0.03
-            self._emis_ox = 0.6
+            self._resist      =     2.65e-8
+            self._cond        =     1.0/self._resist
+
+            self._emis_pol    =     0.03
+            self._emis_ox     =     0.6
 
 
         elif matname == 'OFE_Cu':
             self._name = 'Oxygen Free Electronic Copper'
-            self._density = 8960
+
+            self._density     =     8960.0
+            self._specheat    =     385.0
+
+
+        elif matname == 'BeCu':
+            self._name = 'Beryllium Copper'
+
+            self._density     =     8250.0
+            self._specheat    =     420.0
+            self._tmelt       =     1139.0
+            self._resist      =     8.21e-8
 
 
         else:
@@ -47,20 +67,25 @@ class Mat:
 
 
     def knowndata(self):
-        data = self.__dict__
+        data = self.__dict__.copy()
         for attr in data.keys():
-            del data['knownmats']
             if data[attr] is None:
                 del data[attr]
+        del data['MB_INDEX']
         return data
 
 
+    def index(self):
+        return self.MB_INDEX
 
     @property
-    def knownmats(self):
-        return self._knownmats
+    def name(self):
+        return self._name
 
 
+
+
+    ##############  MECHANICAL ########################
     @property
     def density(self):
         if self._density is not None:
@@ -68,12 +93,61 @@ class Mat:
         else:
             raise NoMatDataError(self._name,'density')
 
+
+    ###############  THERMCAL  ########################
+    @property
+    def specheat(self):
+        if self._specheat is not None:
+            return self._specheat
+        else:
+            raise NoMatDataError(self._name,'specific heat')
+
+    @property
+    def tcond(self):
+        if self._tcond is not None:
+            return self._tcond
+        else:
+            raise NoMatDataError(self._name,'thermal conductivity')
+
+
+    @property
+    def tmelt(self):
+        if self._tmelt is not None:
+            return self._tmelt
+        else:
+            raise NoMatDataError(self._name,'melting temperature')
+
+
+    ##############  ELECTRICAL ########################
     @property
     def resist(self):
         if self._resist is not None:
             return self._resist
         else:
             raise NoMatDataError(self._name,'resistivity')
+
+    @property
+    def cond(self):
+        if self._cond is not None:
+            return self._cond
+        else:
+            raise NoMatDataError(self._name,'conductivity')
+
+
+    ##############   OPTICAL   ########################
+    @property
+    def emis_pol(self):
+        if self._emis_pol is not None:
+            return self._emis_pol
+        else:
+            raise NoMatDataError(self._name,'polished emissivity')
+
+    @property
+    def emis_ox(self):
+        if self._specheat is not None:
+            return self._specheat
+        else:
+            raise NoMatDataError(self._name,'oxidized emissivity')
 
 
 
